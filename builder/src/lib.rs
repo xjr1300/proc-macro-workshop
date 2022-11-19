2d31aea1e218bc5be6bd39715692c4073d422e07
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-11-19 16:47:07 +0900

    builder 03

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -14,6 +14,28 @@ pub fn derive(input: TokenStream) -> TokenStream {
             current_dir: Option<String>,
         }
 
+        impl CommandBuilder {
+            fn executable(&mut self, executable: String) ->&mut Self {
+                self.executable = Some(executable);
+                self
+            }
+
+            fn args(&mut self, args: Vec<String>) -> &mut Self {
+                self.args = Some(args);
+                self
+            }
+
+            fn env(&mut self, env: Vec<String>) -> &mut Self {
+                self.env = Some(env);
+                self
+            }
+
+            fn current_dir(&mut self, current_dir: String) -> &mut Self {
+                self.current_dir = Some(current_dir);
+                self
+            }
+        }
+
         impl Command {
             fn builder() -> CommandBuilder {
                 CommandBuilder {
