e675503fc9cd76d7b63bb7877f31073c46982bc5
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-11-19 16:24:04 +0900

    builder 02

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,9 +1,30 @@
 use proc_macro::TokenStream;
+use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let _input = parse_macro_input!(input as DeriveInput);
 
-    proc_macro::TokenStream::new()
+    let expanded = quote! {
+        pub struct CommandBuilder {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
+
+        impl Command {
+            fn builder() -> CommandBuilder {
+                CommandBuilder {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
+            }
+        }
+    };
+
+    proc_macro::TokenStream::from(expanded)
 }
